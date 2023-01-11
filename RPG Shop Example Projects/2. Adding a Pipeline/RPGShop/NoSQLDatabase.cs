47ca12424cd795fe3ffd3928a3da9561bcfa65e9
Rowan Lea <rowan.lea@codurance.com>
2023-01-11 12:01:18 +0000

    Working again

--- RPG Shop Example Projects/2. Adding a Pipeline/RPGShop/NoSQLDatabase.cs
+++ RPG Shop Example Projects/2. Adding a Pipeline/RPGShop/NoSQLDatabase.cs
@@ -10,7 +10,7 @@ namespace RPGShop
         internal NoSQLDatabase()
         {
             _dbClient = new MongoClient("mongodb://localhost:27017");
-            _database = _dbClient.GetDatabase("Sales");fail
+            _database = _dbClient.GetDatabase("Sales");
         }
 
         internal void MakeSale(Sale sale)
