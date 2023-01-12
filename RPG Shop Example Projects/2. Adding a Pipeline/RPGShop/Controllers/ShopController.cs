a1ab60036cfdd32607b479301e144ade3b09612e
Rowan <rowan.lea92@gmail.com>
2023-01-12 13:04:20 +0000

    Breaking on purpose

--- RPG Shop Example Projects/2. Adding a Pipeline/RPGShop/Controllers/ShopController.cs
+++ RPG Shop Example Projects/2. Adding a Pipeline/RPGShop/Controllers/ShopController.cs
@@ -14,7 +14,7 @@ namespace RPGShop.Controllers
         {
             _sqlDb = new SQLDatabase();
             _noSqlDb = new NoSQLDatabase();
-            _logger = new FileLogger("C:/RPGLogs/log.txt");
+            _logger = new FileLogger("C:/RPGLogs/log.txt");gfedrgfedg
         }
 
         [HttpGet]
