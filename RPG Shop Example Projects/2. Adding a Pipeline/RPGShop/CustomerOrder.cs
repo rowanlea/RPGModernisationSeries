6e68dbbbfca616313001d94f149f2a4088db78af
Rowan <rowan.lea92@gmail.com>
2023-01-12 11:57:06 +0000

    Removed test project

--- RPG Shop Example Projects/2. Adding a Pipeline/RPGShop/CustomerOrder.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RPGShop
-{
-    public class CustomerOrder
-    {
-        public List<Item> Items { get; set; }
-        public CustomerDetails CustomerDetails { get; set; }
-        public bool IsTab { get; set; }
-    }
-}
