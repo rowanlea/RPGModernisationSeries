10a71ac1d46dae928cae510bde7cb25b97c10589
Rowan <rowan.lea92@gmail.com>
2023-01-12 13:02:28 +0000

    Fixed issue

--- RPG Shop Example Projects/2. Adding a Pipeline/RPGShop/Controllers/ShopController.cs
+++ RPG Shop Example Projects/2. Adding a Pipeline/RPGShop/Controllers/ShopController.cs
@@ -14,7 +14,7 @@ namespace RPGShop.Controllers
         {
             _sqlDb = new SQLDatabase();
             _noSqlDb = new NoSQLDatabase();
-            _logger = new FileLogger("C:/RPGLogs/log.txt");gedrgerdfgfer
+            _logger = new FileLogger("C:/RPGLogs/log.txt");
         }
 
         [HttpGet]
